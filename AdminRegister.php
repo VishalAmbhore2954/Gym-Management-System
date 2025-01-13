193596c479ac4081b1b0b98d83704b42832dfc13
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-01-13 15:19:41 +0530

    Modified file AdminRegister.html to AdminRegister.php

---
+++ AdminRegister.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="AdminRegister.css">
+</head>
+<body>
+    <div class="logo">
+        <img src="logo.png" alt="">
+        <label for="">FITNESS KING</label>
+    </div>
+</div>
+    <div class="container">
+        <form action="">
+            <h2>REGISTRATION</h2>
+        <div class="form-group">
+            <label for="">Enter First Name :</label><br>
+            <input type="text" name="fname" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Last Name :</label><br>
+            <input type="text" name="lname" required>
+        </div>
+        <div class="form-group">
+            <label for="">Email ID :</label><br>
+            <input type="text" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Mobile Number :</label><br>
+            <input type="text" name="mno" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Username :</label><br>
+            <input type="text" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Password :</label><br>
+            <input type="password" name="password" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Confirm Password :</label><br>
+            <input type="password" name="cpassword" required>
+        </div>
+        <input type="submit" name="submit" id="btn" value="REGISTER">
+        <div class="nxt">
+            <p>Already have account?</p><a href="AdminLogin.html">Login Here</a>
+        </div>
+    </div>
+        </form>
+    <div class="logo">
+        <img src="logo.png" alt="">
+        <label for="">FITNESS KING</label>
+    </div>
+</body>
+</html>
