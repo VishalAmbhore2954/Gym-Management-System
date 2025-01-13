83a417cd40871be460b3453c4e5920de4edfa031
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-01-13 18:57:21 +0530

    created user registration form

---
+++ UserForm.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="UserForm.css">
+</head>
+<body>
+    <!-- <div class="logo">
+        <img src="logo.png" alt="">
+        <label for="">FITNESS KING</label>
+    </div> -->
+</div>
+    <div class="container">
+        <form method="POST">
+            <h2><img src="logo.png" alt="">REGISTER USER<img src="logo.png" alt=""></h2>
+        <div class="form-group">
+            <label for="">Enter First Name :</label><br>
+            <input type="text" name="fname" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Last Name :</label><br>
+            <input type="text" name="lname" required>
+        </div>
+        <div class="form-group">
+            <label for="">Email ID :</label><br>
+            <input type="text" name="email" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Mobile Number :</label><br>
+            <input type="text" name="mno" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Address :</label><br>
+            <input type="text" name="username" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Duration in Month :</label><br>
+            <input type="text" name="password" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Total Fee :</label><br>
+            <input type="password" name="cpassword" required>
+        </div>
+        <div class="form-group">
+            <label for="">Enter Credited Fee :</label><br>
+            <input type="password" name="cpassword" required>
+        </div>
+        <div id="gender">
+            <label for="">GENDER :</label>
+            <input type="radio" name="gender" value="Male">  Male
+            <input type="radio" name="gender" value="Female">  Female
+        </div>
+        <input type="submit" name="submit" id="btn" value="REGISTER">
+    </div>
+        </form>
+    <!-- <div class="logo">
+        <img src="logo.png" alt="">
+        <label for="">FITNESS KING</label>
+    </div> -->
+</body>
+</html>
