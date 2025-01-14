225a3c5e1213186cec464911e82678a503cf1329
VishalAmbhore2954 <vishalambhore2954@gmail.com>
2025-01-15 00:22:37 +0530

    Created admin home page

---
+++ AdminHome.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="AdminHome.css">
+</head>
+<body>
+<div class="navbar">
+        <img src="logo.png" alt="Logo" id="logo">
+        <span id="logoname">
+            FITNESS KING
+        </span>
+        <a href="DisplayCust.php">All Customers</a>
+        <a href="RemFee.php">Collect Fees</a>
+        <a href="feeChart.html">Fees Chart</a>
+        <a href="AdminLogin.php">Log out</a>
+</div>
+<div class="body2">
+    <img src="logo.png" alt="Logo">
+    <a id="addBtn" href="UserForm.php">Add customer<a>
+</div>
+<div class="footer">
+    &#169; All Right Reserved
+</div>
+</body>
+</html>
+
